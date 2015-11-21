398d4e2d2d9c9106e7932d3fb5051a72bcc11e24
Ruihong Huang <ruihong.huang@gmail.com>
2015-11-21 12:04:12 +0000

    read multiple day data

--- lobster.py
+++ lobster.py
@@ -3,14 +3,16 @@ __author__ = 'Ruihong Huang'
 from pandas import DataFrame, read_csv, concat
 from os import path
 import numpy as np
+from datetime import timedelta
 
 
-class Lobster:
+class LobsterData:
     def __init__(self):
         self.order_books = None
         self.messages = DataFrame()
+        self.level = 0
 
-    def read_files(self, order_book_file, message_file, append=True):
+    def read_single_day_data(self, order_book_file, message_file, append=True, convert_time=False):
         file_name = path.basename(order_book_file)
         file_base = path.splitext(file_name)[0]
         mydate = np.datetime64('{0}T00:00-0000'.format(file_base.split('_')[1]))
@@ -18,37 +20,50 @@ class Lobster:
         myorderbooks = read_csv(order_book_file, header=None)
         mymessages = read_csv(message_file, header=None)
         columns = []
-        for iCol in range(len(myorderbooks.columns) / 4):
+        for iCol in range(int(len(myorderbooks.columns) / 4 + 0.5)):
             columns.append('Ask_Price_{0}'.format(iCol + 1))
-            columns .append('Ask_Size_{0}'.format(iCol + 1))
-            columns .append('Bid_Price_{0}'.format(iCol + 1))
-            columns .append('Bid_Size_{0}'.format(iCol + 1))
+            columns.append('Ask_Size_{0}'.format(iCol + 1))
+            columns.append('Bid_Price_{0}'.format(iCol + 1))
+            columns.append('Bid_Size_{0}'.format(iCol + 1))
         myorderbooks.columns = columns
         mymessages.columns = ['Time', 'Event', 'Order_ID', 'Size', 'Price', 'Direction']
-        mymessages.Time *= 1e9
-        timestamps = []
-        for iRow in range(mymessages.shape[0]):
-            timestamps.append(mydate + np.timedelta64(int(mymessages.Time[iRow]), 'ns'))
+        mymessages['Date'] = mydate
+        if convert_time:
+            mymessages.Time = mymessages.Time.apply(lambda x: mydate + np.timedelta64(int(x * 1e9), 'ns'))
 
-        myorderbooks.index = timestamps
-        mymessages.index = timestamps
+        mymessages.set_index(['Date', 'Time'], inplace=True)
+        myorderbooks.index = mymessages.index
 
         if append:
-            self.order_books = concat([self.order_books,myorderbooks], axis=0)
-            self.messages = concat([self.messages,mymessages], axis=0)
+            self.order_books = concat([self.order_books, myorderbooks], axis=0)
+            self.messages = concat([self.messages, mymessages], axis=0)
         else:
             self.order_books = myorderbooks
             self.messages = mymessages
+        self.level = int(self.order_books.shape[1] / 4 + 0.5)
+
+    def read_period_data(self, ticker, level, start_date, end_date, data_path='./', **kwargs):
+        my_date = start_date
+        while my_date <= end_date:
+            print('read {0}'.format(my_date.strftime('%Y-%m-%d')))
+            order_book_file = "{0}/{1}_{2}_34200000_57600000_orderbook_{3}.csv".format(data_path, ticker,
+                                                                                       my_date.strftime('%Y-%m-%d'),
+                                                                                       level)
+
+            message_file = "{0}/{1}_{2}_34200000_57600000_message_{3}.csv".format(data_path, ticker,
+                                                                                       my_date.strftime('%Y-%m-%d'),
+                                                                                       level)
+            self.read_single_day_data(order_book_file, message_file, **kwargs)
+            my_date += timedelta(1)
 
     def get_number_of_record(self):
         return self.order_books.shape[0]
 
     def get_number_of_level(self):
-        return int(self.order_books.shape[1] / 4 + 0.5)
+        return self.level
 
     def bind_trades(self):
         pass
 
-    def plot(self):
+    def plot_orderbook(self):
         pass
-
--- test_lobster.py
+++ test_lobster.py
@@ -1,16 +1,27 @@
 __author__ = 'Ruihong Huang'
+from datetime import datetime
+from lobster import LobsterData
 
-from lobster import Lobster
 
+def test_read_single_date_files():
+    book_file = 'sample_data/AMZN_2012-06-21_34200000_57600000_orderbook_10.csv'
+    message_file = 'sample_data/AMZN_2012-06-21_34200000_57600000_message_10.csv'
 
-def test_read_files():
-    book_file = 'sample_data/AMZN_2015-07-02_34200000_57600000_orderbook_5.csv'
-    message_file = 'sample_data/AMZN_2015-07-02_34200000_57600000_message_5.csv'
-
-    lob = Lobster()
-    lob.read_files(order_book_file=book_file, message_file=message_file)
+    lob = LobsterData()
+    lob.read_single_day_data(order_book_file=book_file, message_file=message_file)
     print(lob.get_number_of_record())
 
 
-test_read_files()
+def test_read_period_data():
+    directory = '/home/ruihong/lobstertest'
+    ticker = 'AMZN'
+    start_date = datetime(2015, 10, 5)
+    end_date = datetime(2015, 10, 9)
+    level = 10
 
+    lob = LobsterData()
+    lob.read_period_data(ticker, level, start_date, end_date, data_path=directory)
+    print(lob.get_number_of_record())
+
+
+test_read_period_data()
